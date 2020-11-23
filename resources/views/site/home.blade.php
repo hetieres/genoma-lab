0c34fd04366ad74f074515e98ca2ea881cb38eba
Ricardo Feliciano Gonçalves <rgoncalves@fapesp.br>
2020-11-23 19:34:53 -0300

    Font Akkurat

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -135,9 +135,9 @@
                             <div class="card mb-4 shadow-sm" style="border: 1px solid #000000;">
                                 <a href="{{ $item->link() }}">
                                     <div class="card-body">
-                                        <p class="TextLimitedProjects" style="color: #1e359c;font-weight: 450;height: 92px;">
+                                        <p class="TextLimitedProjects" style="color: #1e359c;font-weight: 450;height: 120px;">
                                             {{ $item->title }}</p>
-                                        <p class="card-text" style="height: 120px;">
+                                        <p class="card-text" style="height: 105px;">
                                             {!!html_entity_decode(str_replace("\n", "<br>", $item->summary))!!}
                                         </p>
                                     </div>
