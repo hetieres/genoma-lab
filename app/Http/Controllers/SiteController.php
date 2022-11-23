de8252c643dc9992ccd47d3924493c7ecb67b537
João Hetiéres <hetieres@gmail.com>
2022-11-23 09:00:36 -0300

    [update] - email

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -197,6 +197,7 @@ class SiteController extends Controller
             $mail->addReplyTo($request->email, $request->nome);
             $mail->AddAddress('heitor.shimizu@gmail.com');
             $mail->AddAddress('hetieres@gmail.com');
+            $mail->AddAddress('especialista_cegh@ib.usp.br');
 
             if (isset($_FILES['anexo']) && $_FILES['anexo']['error'] == UPLOAD_ERR_OK) {
                 $mail->AddAttachment($_FILES['anexo']['tmp_name'],
@@ -248,6 +249,8 @@ class SiteController extends Controller
             $mail->Body =  $conteudo;
             $mail->addReplyTo($request->email, $request->nome);
             $mail->AddAddress('heitor.shimizu@gmail.com');
+            $mail->AddAddress('hetieres@gmail.com');
+            $mail->AddAddress('especialista_cegh@ib.usp.br');
             if(!$mail->Send()) {
                 $this->data['text'] = '<p>Erro ao enviar e-mail:</p><p>'. $mail->ErrorInfo .'</p>';
             } else {
